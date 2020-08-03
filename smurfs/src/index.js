2de3f1cc4f860bbd663cca2b8ba665b97b97fff7
DanielleMathey95 <danielle.mathey@yahoo.com>
2020-08-02 18:12:02 -0700

    files and folders added

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 ReactDOM.render(<App />, document.getElementById("root"));
